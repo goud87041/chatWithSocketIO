19c531d52733a62c095f40efaba08b474223e7b7
abhishekgoud7 <abhishek.goud@bosonwebtech.com>
2026-08-04 20:43:53 +0530

    connect the DB and server

---
+++ backend/src/config/DB_connection.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const connetDB  = async ()=>{
+    try{
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URL!);
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+    }catch(err){
+        console.error(err);
+    }
+}
+
+export default connetDB;
---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import connectDB from "./config/DB_connection.js";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});

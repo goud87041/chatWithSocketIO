b713cea7ab7ee1bac0936ad28dde66837e08743d
abhishekgoud7 <abhishek.goud@bosonwebtech.com>
2026-09-01 15:24:05 +0530

    feat: initialize backend project with Express, Socket.io, and MongoDB connection configuration

--- backend/src/config/DB_connection.ts
+++ backend/src/config/DB_connection.ts
@@ -1,10 +1,10 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connetDB  = async ()=>{
-    try{
+const connetDB = async () => {
+    try {
         const connectionInstance = await mongoose.connect(process.env.MONGO_URL!);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
-    }catch(err){
+    } catch (err) {
         console.error(err);
     }
 }
